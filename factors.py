c61f380b30fb7da53a09870f8161ceedd4dabf8b
amosmj <amosmj@gmail.com>
2017-09-03 12:26:22 -0500

    uploading the file
    the file I used to compare Ardy's and my work.

---
+++ factors.py
@@ -0,0 +1,165 @@
+# A and B are strings of space delimmited numbers
+# n and m are the size of A and B 1 - 10
+# components of A and B are 1 - 100
+# elements of A are all factors of x while x is a factor of all elements of B
+# output is a count of xs
+import random
+
+def sample_given():
+    sample_ouput = []
+    length_line = '2 3'
+    sample_ouput.append(length_line)
+    A_line = ['2','4']
+    sample_ouput.append(A_line)
+    B_line = ['16', '32', '96']
+    sample_ouput.append(B_line)
+    return sample_ouput
+
+def generate_random():
+    length_line = str(random.randint(1,10)) + ' ' + str(random.randint(1,10))
+    random_output=[]
+    random_output.append(length_line)
+    line_length = int(length_line.split()[0])
+    A_list = []
+    B_list = []
+    for item in range(line_length):
+        A_list.append(str(random.randint(1,100)))
+    random_output.append(A_list)
+    a_max = int(max(A_list))
+    line_length = int(length_line.split()[1])
+    for item in range(line_length):
+        B_list.append(str(random.randint(a_max,100)))
+    random_output.append(B_list)
+    return random_output
+    
+def solve(input_list):
+    output_text = ''
+    if int(max(input_list[1])) <= int(min(input_list[2])):
+        #print('possible')
+        #find denominators of B
+        denom_list = []
+        final_list = []
+        #for every number in our second list, find the common denominators
+        for a_number in range(1, int(min(input_list[2]))+1):
+            denom_bool = True
+            #this could be shortened by someone smarter than me
+            #once a number is found to not be a common denominator
+            #this loop should be abandon
+            for other_number in input_list[2]:
+                #print (other_number + ' % ' + str(a_number) +  ' = ' + str(int(other_number)%a_number))
+                if int(other_number )% a_number != 0:
+                    denom_bool = False
+            #if no number failed the test, add it to the list of potentials
+            if denom_bool == True:
+                denom_list.append(a_number)
+        #printing list for testing
+        #print(denom_list)
+        #print(input_list[1])
+        #now tht I have all the common denominators of B
+        #I want to test them against A
+        #begin by throwing out everything less than the max of A
+        #removing causes weirdness to happen so I'm building a
+        # new list
+        valid_denoms = []
+        for denom in denom_list:
+            #print(denom_list)
+            #print(denom)
+            if denom >= int(max(input_list[1])):
+                valid_denoms.append(denom)
+        #print(valid_denoms)
+        #now test what remains
+        for denom_item in valid_denoms:
+            factor_bool = True
+            for input_item in input_list[1]:
+                if denom_item % int(input_item) != 0:
+                    factor_bool = False
+            if factor_bool == True:
+                final_list.append(denom_item)
+        #print(final_list)
+        output_text = len(final_list)
+
+    else:
+        output_text = 'not possible'
+    return output_text
+
+def getTotalX(a, b):
+    lis1 = []
+    finalCount = 0
+    print('for loop mins', min(a), min(b))
+    for elem in range(min(a), min(b)+1):
+        print('elem: ', elem)
+        countFirst = 0
+        countSecond = 0
+        print('for loop i: ', range(0,len(a)))
+        for i in range(0, len(a)):
+            print('elem % a[i]: ', elem,' % ', a[i],' = ',elem % a[i])
+            print('countfirst = ', countFirst)
+            if elem % a[i] == 0:
+                print('got mod 0')
+                countFirst +=1
+        if countFirst == len(a):
+            for el in range(0, len(b)):
+                print('b[el] % elem: ', b[el],' % ', elem,' = ',b[el] % elem)
+                if b[el] % elem == 0:
+                    print('got mod2 again')
+                    countSecond +=1
+            if countSecond == len(b):
+                print('thats a keeper', elem)
+                finalCount +=1
+    return finalCount
+
+def rebuild_to_be_faster(input_list):
+    a_list = list(map(int,input_list[1]))
+    b_list = list(map(int,input_list[2]))
+    range_of_factors = [max(a_list),min(b_list)]
+    possible_factors = []
+    currect_factor = range_of_factors[0]
+    multiplier = 1
+    print('A list: ', a_list, ' B list: ', b_list, ' range: ',range_of_factors)
+    while currect_factor <= range_of_factors[1]:
+        possible_factors.append(currect_factor)
+        multiplier = multiplier + 1
+        currect_factor = range_of_factors[0] * multiplier
+        print('possible factors: ',possible_factors,)
+    #possible factor list built, now test against list A
+    passed_test_a = []
+    for possibility in possible_factors:
+        in_the_running = True
+        for a_item in a_list:
+            print('test a: ', possibility,' % ', a_item, ' = ', possibility % a_item )
+            if possibility % a_item != 0:
+                in_the_running = False
+        if in_the_running == True:
+            passed_test_a.append(possibility)
+    print('passed test a: ', passed_test_a)
+    if len(passed_test_a) > 0:
+        factors = []
+        for still_possible in passed_test_a:
+            in_the_running = True
+            for b_item in b_list:
+                print('test b: ', b_item , ' % ', still_possible, ' = ', b_item%still_possible)
+                if b_item % still_possible != 0:
+                    in_the_running = False
+            if in_the_running == True:
+                factors.append(still_possible)
+        print(factors)
+    else:
+        factors = []
+    return len(factors)
+
+foo = sample_given()
+#print(rebuild_to_be_faster(foo))
+#print('my version: ',solve(foo))
+#print('Ardys version: ', getTotalX(list(map(int,foo[1])),list(map(int,foo[2]))))
+counter = 0
+while counter < 10:
+    foo = generate_random()
+    #print(foo)
+    my_version = rebuild_to_be_faster(foo)
+    #ardys_version = getTotalX(list(map(int,foo[1])),list(map(int,foo[2])))
+    if my_version != 0:
+    #    if my_version != ardys_version:
+    #        print('RED ALERT')
+    #    print(foo)
+    #    print('mine: ', my_version, ' ardys: ', ardys_version)
+        counter = counter + 1
